b422876d2a7683520f5a725fb2c8328c74c4abcf
Sandhykaarya <92094871+Sandhykaarya@users.noreply.github.com>
2023-04-15 15:50:48 +0700

    update method calculatetax
    Perubahan method calculatetax dengan menambahkan beberapa method baru untuk mengurangi panjangnya method calcuatetax

--- TaxCalculator/src/lib/TaxFunction.java
+++ TaxCalculator/src/lib/TaxFunction.java
@@ -1,38 +1,31 @@
 package lib;
 
 public class TaxFunction {
-
-	
-	/**
-	 * Fungsi untuk menghitung jumlah pajak penghasilan pegawai yang harus dibayarkan setahun.
-	 * 
-	 * Pajak dihitung sebagai 5% dari penghasilan bersih tahunan (gaji dan pemasukan bulanan lainnya dikalikan jumlah bulan bekerja dikurangi pemotongan) dikurangi penghasilan tidak kena pajak.
-	 * 
-	 * Jika pegawai belum menikah dan belum punya anak maka penghasilan tidak kena pajaknya adalah Rp 54.000.000.
-	 * Jika pegawai sudah menikah maka penghasilan tidak kena pajaknya ditambah sebesar Rp 4.500.000.
-	 * Jika pegawai sudah memiliki anak maka penghasilan tidak kena pajaknya ditambah sebesar Rp 4.500.000 per anak sampai anak ketiga.
-	 * 
-	 */
-	
 	
 	public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, boolean isMarried, int numberOfChildren) {
 		
-		int tax = 0;
+		int tax;
 		
-		if (numberOfMonthWorking > 12) {
-			System.err.println("More than 12 month working per year");
-		}else if (numberOfChildren > 3) {
-			numberOfChildren = 3;
-		}
+		checkMonth(numberOfMonthWorking);
+		numberOfChildren = checkChildren(numberOfChildren);
 
 		tax = married(monthlySalary, otherMonthlyIncome, numberOfMonthWorking, deductible, isMarried, numberOfChildren);
 		
-		if (tax < 0) {
-			return 0;
-		}else {
-			return tax;
+		return checkTax(tax);
+	}
+
+	public static int checkChildren(int numberOfChildren){
+		if (numberOfChildren > 3) {
+			return 3;
+		}else{
+			return numberOfChildren;
 		}
-			 
+	}
+
+	public static void checkMonth(int numberOfMonthWorking){
+		if (numberOfMonthWorking > 12) {
+			System.err.println("More than 12 month working per year");
+		}
 	}
 
 	public static int married(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, boolean isMarried, int numberOfChildren){
@@ -40,6 +33,14 @@ public class TaxFunction {
 			return (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - (54000000 + 4500000 + (numberOfChildren * 1500000))));
 		}else {
 			return (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - 54000000));
+		}
+	}
+
+	public static int checkTax(int tax){
+		if (tax < 0) {
+			return 0;
+		}else {
+			return tax;
 		}
 	}
 	
